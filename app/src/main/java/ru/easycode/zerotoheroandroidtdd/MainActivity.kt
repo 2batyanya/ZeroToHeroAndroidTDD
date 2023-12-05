57205a9d7a7406a30e8fd1977e0bec38762b9039
Ilya <2batyanya@gmail.com>
2023-12-05 15:10:45 +0300

    task 021 watch input solved by TextWatcher OOP

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.core.widget.addTextChangedListener
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
@@ -9,15 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.rootLayout)
+        setContentView(binding.root)
+
+        val textWatcher = object : SimpleTextWatcher() {
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
+            }
+        }
 
         binding.actionButton.setOnClickListener {
             binding.titleTextView.text = binding.inputEditText.text
             binding.inputEditText.setText("")
         }
 
-        binding.inputEditText.addTextChangedListener {
-            binding.actionButton.isEnabled = binding.inputEditText.text.toString().length >= 3
-        }
+        binding.inputEditText.addTextChangedListener(textWatcher)
     }
+}
+
+// can be moved to core package
+private abstract class SimpleTextWatcher : TextWatcher {
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+    override fun afterTextChanged(s: Editable?) = Unit
 }
